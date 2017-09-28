61c15263c4c634dd2902d7ad2660021acd2a8aa4
aeecheverry <andres.e.e.o97@outlook.com>
2017-09-28 00:18:17 -0500

    Footer y soluciones
    Rediseño el footer, soluciono error del APUserController tenia un parametro id que no se estaba usando

--- ShopBay/Controllers/APUserController.cs
+++ ShopBay/Controllers/APUserController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ShopBay.Models;
 namespace Intento.Controllers
 {
     public class APUserController : Controller
     {
-     
-        public ActionResult APUser(int id)
+        ShopBayEntities db = new ShopBayEntities();
+        public ActionResult APUser()
         {
+            ViewBag.Categories = db.Category.ToList();
             return View();
         }
 
