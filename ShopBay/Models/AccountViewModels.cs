d6d70c3d8d6f5cc63a3f2c2644d3b7d8cad055e1
aeecheverry <andres.e.e.o97@outlook.com>
2017-09-29 16:16:24 -0500

    Revert "Modificaciones"
    This reverts commit cc5e4d1ec6807fb55ad13cc1a62bc9782864ea5d.

--- ShopBay/Models/AccountViewModels.cs
+++ ShopBay/Models/AccountViewModels.cs
@@ -3,7 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ShopBay.Models
 {
-    
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
 
     public class SendCodeViewModel
     {
