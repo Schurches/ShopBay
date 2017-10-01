a5c05907cb62182d767a12e0aa94219737430742
Steven Iglesias <steveniglesias@hotmail.com>
2017-10-01 01:15:25 -0500

    Log In Implemented
    -Implemented Log in
    -Also implemented compatibility with profile
    -_LoginPartial view differs when user is logged in and logged off
    -CLASS USERS (FROM MODELS) now validates information fields on login and
    register.

--- ShopBay/Controllers/SignInController.cs
+++ ShopBay/Controllers/SignInController.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopBay.Models;
+using System.Web.Security;
 
 namespace ShopBay.Controllers
 {
     public class SignInController : Controller
     {
+
+        ShopBayEntities bd = new ShopBayEntities();
         // GET: SignIn
         public ActionResult SignIn()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ValidarLogin(Users usuario)
+        {
+            //if (ModelState.IsValid)
+            //{
+                var user = bd.Users.ToList();
+                foreach (var item in user)
+                {
+                    if (item.Username == usuario.Username)
+                    {
+                        if (item.Password.Equals(usuario.Password))
+                        {
+                            FormsAuthentication.SetAuthCookie(usuario.Username, true);
+                            Session["UserID"] = item.UserID;
+                            Session["Username"] = item.Username;
+                            Session["Type"] = item.Type;
+                            Session["Telephone"] = item.Telephone;
+                            Session["Rate"] = item.Rate;
+                            Session["Mail"] = item.Mail;
+                            Session["Information"] = item.Information;
+                            Session["AccMoney"] = item.AccMoney;
+                            Session["Image"] = item.Image;
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                }
+                ModelState.AddModelError("", "Wrong username or password");
+            //}
+            return RedirectToAction("SignIn","SignIn");
+        }
+
+        public ActionResult Salir()
+        {
+            FormsAuthentication.SignOut();
+            Session["UserID"] = null;
+            Session["Username"] = null;
+            Session["Type"] = null;
+            Session["Telephone"] = null;
+            Session["Rate"] = null;
+            Session["Mail"] = null;
+            Session["Information"] = null;
+            Session["AccMoney"] = null;
+            Session["Image"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }
--- ShopBay/Models/Users.cs
+++ ShopBay/Models/Users.cs
@@ -11,7 +11,8 @@ namespace ShopBay.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Users
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -28,17 +29,40 @@ namespace ShopBay.Models
         }
     
         public int UserID { get; set; }
+        [Required(ErrorMessage = "Enter your username", AllowEmptyStrings = false)]
+        [Display(Name = "Username")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username must NOT contain white spaces or @")]
+        [StringLength(50, ErrorMessage = "The username must be at least {2} characters long.", MinimumLength = 3)]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Enter your name", AllowEmptyStrings = false)]
+        [Display(Name = "Name")]
+        [StringLength(50, ErrorMessage = "The name must be at least 50 characters characters long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "name must only contain letters")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your password", AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long. And must not be pure blanks", MinimumLength = 3)]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Type { get; set; }
+        [Required(ErrorMessage = "Enter your name", AllowEmptyStrings = false)]
+        [Display(Name = "Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "name must only contain letters")]
         public string Information { get; set; }
+        [Required(ErrorMessage = "Your must provide a PhoneNumber")]
+        [Display(Name = "Telephone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
         public string Telephone { get; set; }
+        [Required(ErrorMessage = "Your Email address")]
+        [Display(Name = "Mail")]
+        [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
         public string Mail { get; set; }
         public Nullable<double> Rate { get; set; }
         public Nullable<int> AccMoney { get; set; }
         public byte[] Image { get; set; }
-    
+
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Movements> Movements { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
