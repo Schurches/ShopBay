ea5e2fc7d3591e6744825632df84306b90a01ebc
aeecheverry <andres.e.e.o97@outlook.com>
2017-10-02 18:43:49 -0500

    Comentario bootstrap
    Ya funcionan los comentarios
    Agregué las redirecciones
    Y contenedores de boostrap innecesarios

--- ShopBay/Controllers/ProfileUController.cs
+++ ShopBay/Controllers/ProfileUController.cs
@@ -9,38 +9,54 @@ namespace ShopBay.Controllers
     public class ProfileUController : Controller
     {
         ShopBayEntities db = new ShopBayEntities();
+        static int userRID = 0;
         // GET: Profile
-        public ActionResult ProfileU(int id)
+        public ActionResult ProfileU(int? id)
         {
-            var user = db.Users.Find(id);
-            ViewBag.Comentaries = db.ProfileCommentary.Where(pc => pc.UserID == user.UserID).ToList();
-            ViewBag.User = user;
+            if (id != null)
+            {
+                var userR = db.Users.Find((int)id);
+                ViewBag.Comentaries = db.ProfileCommentary.Where(pc => pc.RatedUserID == userR.UserID).ToList();
+                ViewBag.User = userR;
+                userRID = (int)id;
+            }
+            else
+            {
+                return RedirectToAction("SignIn", "SignIn");
+            }
             return View();
         }
 
         [HttpPost]
         public ActionResult ProfileU(ProfileCommentary comentary)
         {
-            var user= db.Users.Find(comentary.UserID);
-            var userR= db.Users.Find(comentary.RatedUserID);
-            ViewBag.Comentaries = db.ProfileCommentary.Where(pc => pc.UserID == comentary.UserID).ToList();
-            ViewBag.User = user;
-            if (ModelState.IsValid)
+            if (Session["UserID"] != null)
             {
-                comentary.ProfileCommentaryID = db.ProfileCommentary.Count() + 1;
-                comentary.UserID = comentary.UserID;
-                comentary.RatedUserID = comentary.RatedUserID;
-                comentary.Rate = Int32.Parse(Request.Form["rateUserC"]);
-                //No se si estos manden error del foreing key, el modelo los tiene y los pongo para llenar todo los campos
-                comentary.Users = user;
-                comentary.Users1 = userR;
-                
-                db.ProfileCommentary.Add(comentary);
-                db.SaveChanges();
-                ModelState.Clear();
+                var user = db.Users.Find(Int32.Parse(Session["UserID"].ToString()));
+                var userR = db.Users.Find(userRID);
+                ViewBag.User = userR;
+                if (ModelState.IsValid)
+                {
+                    comentary.ProfileCommentaryID = db.ProfileCommentary.Count() + 1;
+                    comentary.UserID = user.UserID;
+                    comentary.RatedUserID = userR.UserID;
+                    comentary.Rate = Int32.Parse(Request.Form["rateUserC"]);
+                    //No se si estos manden error del foreing key, el modelo los tiene y los pongo para llenar todo los campos
+                    comentary.Users = user;
+                    comentary.Users1 = userR;
+                    var r = db.Users.Find(userR.UserID).Rate;
+                    db.Users.Find(userR.UserID).Rate = (r + comentary.Rate) / 2;
+                    db.ProfileCommentary.Add(comentary);
+                    db.SaveChanges();
+                    ViewBag.Comentaries = db.ProfileCommentary.Where(pc => pc.RatedUserID == userR.UserID).ToList();
+                    ModelState.Clear();
+                }
             }
-            
-            return View(comentary);
+            else
+            {
+                return RedirectToAction("SignIn", "SignIn");
+            }
+            return View();
         }
         
     }
